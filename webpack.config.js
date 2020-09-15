e0bdecdb9feb4c510eb728973c578fe0cf5c684f
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-09-15 20:36:25 +0100

    - Updated Dependencies / Need to Check if I didn't break anything inadvertently

--- webpack.config.js
+++ webpack.config.js
@@ -22,14 +22,7 @@ module.exports = {
           loader: 'css-loader', // translates CSS into CommonJS modules
         }, {
           loader: 'postcss-loader', // Run post css actions
-          options: {
-            plugins: function () { // post css plugins, can be exported to postcss.config.js
-              return [
-                require('precss'),
-                require('autoprefixer')
-              ];
-            }
-          }
+          options: { postcssOptions: { plugins: ['precss', 'autoprefixer'] } }
         }, {
           loader: 'sass-loader' // compiles Sass to CSS
         }]
